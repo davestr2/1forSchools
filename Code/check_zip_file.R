5b6892509a3d4ed831415f448156e58065c79d1a
Dave Stritzinger <davestr2@gmail.com>
2026-08-13 20:37:16 -0400

    This is the initial commit

---
+++ Code/check_zip_file.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(zip)
+
+main_path <- here::here()
+data_path <- paste0(main_path, "/Data")
+video_path <- paste0(main_path, "/Video")
+document <- paste0(main_path, "Documents")
+
+
