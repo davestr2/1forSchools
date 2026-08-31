b7fa162f51a2d360960980ce47732be56bf0083f
Dave Stritzinger <davestr2@gmail.com>
2026-08-31 14:49:42 -0400

    Add some Data files

--- Code/check_zip_file.R
+++ Code/check_zip_file.R
@@ -6,4 +6,15 @@ data_path <- paste0(main_path, "/Data")
 video_path <- paste0(main_path, "/Video")
 document <- paste0(main_path, "Documents")
 
+zip_name <- "2025_all_county_high_level.zip"
+
+file_info <- zip_list(paste0(data_path, zip_name))
+
+str_extract(file_info$filename, "[^.]+$") #|> 
+
+names(file_info)
+
+file_names <- file_info |> 
+  select(filename)
+nrow(file_names)
 
